542ddfab8c8d30863389f34ff31326a07f2dcbd0
Mario Behrendt <m.behrendt@yanted.com>
2013-09-02 20:11:02 +0200

    Possibility to get container instead of JSON string, closes #1

--- test/jbuilder.js
+++ test/jbuilder.js
@@ -1,60 +1,59 @@
 var expect    = require('expect.js');
 var jbuilder  = require('../');
 
-function parsedOutput(output) {
-  return JSON.parse(output);
-}
-
 describe('jbuilder', function() {
-  before(function() {
-    this.user = { name: 'foo' };
-    this.profile = { imagePath: '/0815.jpg' };
+  it('returns a converted json string', function() {
+    var json = jbuilder.encode(function(json) {
+      json.set('name', 'foo');
+    });
+
+    expect(json).to.eql('{"name":"foo"}');
   });
 
   it('adds first level attributes', function() {
     var user = this.user;
 
-    var output = parsedOutput(jbuilder.encode(function(json) {
-      json.set('name', user.name);
-    }));
+    var output = jbuilder.create(function(json) {
+      json.set('name', 'bar');
+    });
 
-    expect(output).to.eql({ name: user.name });
+    expect(output.content).to.eql({ name: 'bar' });
   });
 
   it('adds second and third level attributes', function() {
     var profile = this.profile;
 
-    var output = parsedOutput(jbuilder.encode(function(json) {
+    var output = jbuilder.create(function(json) {
       json.set('profile', function(json) {
-        json.set('imagePath', profile.imagePath);
+        json.set('imagePath', '/0815.jpg');
 
         json.set('chuck', function(json) {
           json.set('name', 'Norris');
         })
       });
-    }));
+    });
 
-    expect(output).to.eql({ profile: {
-      imagePath: profile.imagePath,
+    expect(output.content).to.eql({ profile: {
+      imagePath: '/0815.jpg',
       chuck: { name: 'Norris' }
     }});
   });
 
   it('ignores falsy values', function() {
-    var output = parsedOutput(jbuilder.encode(function(json) {
+    var output = jbuilder.create(function(json) {
       json.ignoreFalsy = true;
       json.set('username', false);
-    }));
+    });
 
-    expect(output).to.eql({});
+    expect(output.content).to.eql({});
   });
 
   it('extracts given keys', function() {
-    var output = parsedOutput(jbuilder.encode(function(json) {
+    var output = jbuilder.create(function(json) {
       json.extract({ one: 1, two: 2, three: 3 }, 'one', 'two');
-    }));
+    });
 
-    expect(output).to.eql({ one: 1, two: 2 });
+    expect(output.content).to.eql({ one: 1, two: 2 });
   });
 
   it('throws if only one parameter is given to json.extract()', function() {
@@ -66,15 +65,15 @@ describe('jbuilder', function() {
   });
 
   it('extracts from arrays', function() {
-    var output = parsedOutput(jbuilder.encode(function(json) {
+    var output = jbuilder.create(function(json) {
       json.extract([{ foo: 1, bar: 1 }, { foo: 2, bar: 2 }, { foo: 3, bar: 3 }], 'foo');
-    }));
+    });
 
-    expect(output).to.eql([{ foo: 1 }, { foo: 2 }, { foo: 3 }]);
+    expect(output.content).to.eql([{ foo: 1 }, { foo: 2 }, { foo: 3 }]);
   });
 
   it('turns given object into array', function() {
-    var output = parsedOutput(jbuilder.encode(function(json) {
+    var output = jbuilder.create(function(json) {
       json.set('users', function(json) {
         json.child(function(json) {
           json.set('name', 'foo');
@@ -83,8 +82,8 @@ describe('jbuilder', function() {
           json.set('bar', 'baz');
         });
       });
-    }));
+    });
 
-    expect(output).to.eql({ users: [{ name: 'foo' }, { bar: 'baz' }]});
+    expect(output.content).to.eql({ users: [{ name: 'foo' }, { bar: 'baz' }]});
   });
 });
